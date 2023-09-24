c5b627d74c61d2600e23c683e5bbeef08cf4c2dd
Stig-Martin Liavåg <stigmartin94@gmail.com>
2023-09-24 19:30:38 +0200

    Add docstring to insights

--- coinbase_insights/insights.py
+++ coinbase_insights/insights.py
@@ -4,6 +4,11 @@ from sklearn.linear_model import LinearRegression
 
 
 def show_runtime_elapsed(df: pd.DataFrame):
+    """Prints current/max timestamp and total runtime of the application in seconds.
+
+    Args:
+        df (pd.DataFrame): Collected feed message data.
+    """
     print("*" * 40)
     print(f"Current timestamp: {df.index.max()}")
     print(
@@ -13,6 +18,16 @@ def show_runtime_elapsed(df: pd.DataFrame):
 
 
 def show_current_bid_ask(df: pd.DataFrame):
+    """Prints the highest bid and lowest ask, with quantities, in the current,
+    most recent feed message. Expects the following DataFrame columns:
+    - highest_bid
+    - highest_bid_quantity
+    - lowest_ask
+    - lowest_ask_quantity
+
+    Args:
+        df (pd.DataFrame): Collected feed message data.
+    """
     current = df.iloc[-1]
 
     print(f"Current highest bid: {current['highest_bid']}")
@@ -23,12 +38,29 @@ def show_current_bid_ask(df: pd.DataFrame):
 
 
 def show_largest_diff_bid_ask(df: pd.DataFrame):
+    """Prints the largest difference between highest ask and
+    lowest ask in the data collected so far. Expects the following
+    DataFrame columns:
+    - diff
+
+    Args:
+        df (pd.DataFrame): Collected feed message data.
+    """
+
     highest_diff = df[df["diff"].abs().max() == df["diff"].abs()].iloc[0]
     print(f"Biggest difference between bid and ask: {highest_diff['diff']:.3f}")
     print("-" * 40 + "\n")
 
 
 def show_average_mid_price(df: pd.DataFrame):
+    """Prints the average mid-price in the last 1, 5 and 15 minutes.
+    Expects the following DataFrame columns:
+    - mid_price
+
+    Args:
+        df (pd.DataFrame): Collected feed message data.
+    """
+
     mid_price = df[["mid_price"]]
     for m in [1, 5, 15]:
         avg_mid_price = (
@@ -39,6 +71,16 @@ def show_average_mid_price(df: pd.DataFrame):
 
 
 def show_forecasts(df: pd.DataFrame):
+    """Prints the forecasted mid-price error in 60 seconds,
+    in addition to the average mid-price forecasting errors in the
+    last 1, 5 and 15 minutes. Expects the following DataFrame columns:
+    - mid_price_pred_60s
+    - forecast_error
+
+    Args:
+        df (pd.DataFrame): Collected feed message data.
+    """
+
     forecast_60s = df.iloc[-1]["mid_price_pred_60s"]
     print(f"Forecast in 60 s: {forecast_60s:.3f}")
 
@@ -53,7 +95,19 @@ def show_forecasts(df: pd.DataFrame):
     print("-" * 40 + "\n")
 
 
-def print_insights(df_output):
+def print_insights(df_output: pd.DataFrame):
+    """Prints insights calculated from the collected data:
+    - Elapsed runtime and current timestamp.
+    - Current highest bid and lowest ask, with quantites.
+    - Largest difference between highest bid and lowest ask seen so far.
+    - Average mid-price in the last 1, 5, 15 minutes.
+    - Forecasted mid-price in 60 seconds.
+    - Forecasting error on predicted mid-price in the last 1, 5, 15 minutes.
+
+    Args:
+        df_output (pd.DataFrame): Collected feed message data.
+    """
+
     print()
 
     show_runtime_elapsed(df_output)
@@ -67,7 +121,17 @@ def print_insights(df_output):
     show_forecasts(df_output)
 
 
-def create_predictor(df) -> LinearRegression:
+def create_predictor(df: pd.DataFrame) -> LinearRegression:
+    """Creates a linear regression model to predict future mid-price values.
+    Expects the following DataFrame columns:
+    - mid_price
+
+    Args:
+        df (pd.DataFrame): Collected feed message data.
+
+    Returns:
+        LinearRegression: Linear model fitted to the mid-price values seen so far.
+    """
     linear_regressor = LinearRegression()
     X = df.index.values.astype("int64").reshape(-1, 1)
     Y = df["mid_price"].values.astype("float64").reshape(-1, 1)
@@ -77,6 +141,25 @@ def create_predictor(df) -> LinearRegression:
 
 
 def calculate_forecasts(df: pd.DataFrame) -> pd.DataFrame:
+    """Trains a new predictor, predicts a forecasted mid-price value
+    60 seconds in the future relative to the most recent timestamp,
+    adds the value to the most recent row, and calculates the forecasting error.
+    Expects the following DataFrame columns:
+    - mid_price
+
+    Assumes that a new row is added to the DataFrame every 5 seconds, as the forecast
+    for 60 seconds ahead is stored in the current row. The forecasted value for the
+    current row therefore will be 12 rows back, i.e. 12 * 5 seconds = 60 seconds ago.
+
+    Args:
+        df (pd.DataFrame): Collected feed message data.
+
+    Returns:
+        pd.DataFrame: Added columns of future mid-price forecasts and errors:
+        - mid_price_pred_60s
+        - mid_price_pred
+        - forecast_error
+    """
     reg = create_predictor(df)
 
     tp_shift = np.array(df.index.shift(60, freq="s").max().value).reshape(-1, 1)
